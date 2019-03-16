34740f3e87d636f3158d5bb8b7233b12cf78b249
Rafael Fueyo-Gomez <rfueyogo@caltech.edu>
2019-03-16 01:59:11 -0700

    committing two random graphs

--- run_voronoi.py
+++ run_voronoi.py
@@ -1,12 +1,12 @@
 import voronoiPlanner as vp 
 import graphGenerator as gg 
 
-# gg.GraphGenerator(30, 30, 0.4, 7, 'random_graph1_30_30.txt')
+# gg.GraphGenerator(100, 100, 0.2, 30, 'random_graph1_100_100.txt')
 
-v = vp.VoronoiPlanner('random_graph1_30_30.txt')
+v = vp.VoronoiPlanner('random_graph1_100_100.txt')
 
 v.pretty_print_graph()
 v.pretty_print_voronoi()
 v.pretty_print_obstacle_wave()
 
-v.pretty_print_path((2, 20), (26, 4))
+v.pretty_print_path((6, 30), (45, 30))
